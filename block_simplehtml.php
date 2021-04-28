3eba54ad6b2b15b3ef8d8f38c6d05e8d7eacbd28
Tom Forsthoevel <tom.forsthoevel@protonmail.com>
2021-04-28 23:49:38 +0200

    1

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -7,14 +7,38 @@ class block_simplehtml extends block_base {
     // will only be closed after there is another function added in the next section.
 
     public function get_content() {
-    if ($this->content !== null) {
+      if ($this->content !== null) {
+        return $this->content;
+      }
+
+      if (! empty($this->config->text)) {
+        $this->content->text = $this->config->text;
+      }
+
+      $this->content         =  new stdClass;
+      $this->content->title  = 'My Block!';
+      $this->content->text   = 'The content of our SimpleHTML block!';
+      $this->content->footer = 'Footer here...';
+
       return $this->content;
     }
 
-    $this->content         =  new stdClass;
-    $this->content->text   = 'The content of our SimpleHTML block!';
-    $this->content->footer = 'Footer here...';
+    public function specialization() {
+      if (isset($this->config)) {
+        if (empty($this->config->title)) {
+            $this->title = get_string('defaulttitle', 'block_simplehtml');
+        } else {
+            $this->title = $this->config->title;
+        }
 
-    return $this->content;
-}
+        if (empty($this->config->text)) {
+            $this->config->text = get_string('defaulttext', 'block_simplehtml');
+        }
+      }
+    }
+
+    public function googleSearch() {
+      $api_url = 'https://cse.google.com/cse.js?cx=902af03543ff6269f';
+      $json_data = file_get_contens($api_url);
+    }
 }
--- lang/en/block_simplehtml.php
+++ lang/en/block_simplehtml.php
@@ -3,3 +3,7 @@ $string['pluginname'] = 'Simple HTML block';
 $string['simplehtml'] = 'Simple HTML';
 $string['simplehtml:addinstance'] = 'Add a new simple HTML block';
 $string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
+$string['blocksettings'] = 'Settings';
+$string['blocktitle'] = 'Block Title';
+$string['blockstring'] = 'Block String';
+$string['defaulttitle'] = 'My Block';
